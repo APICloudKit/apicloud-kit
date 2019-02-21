57b8a2657746f923004e4bcb9773cd5663f4b789
Kai Ye <grepug@163.com>
2019-02-21 11:41:50 +0800

    add apicloud.config.d.ts

---
+++ types/apicloud.config.d.ts
@@ -0,0 +1,42 @@
+export interface apicloudConfig {
+    id: string;
+    version: string;
+    name: {
+        text: string;
+    };
+    description: {
+        text: string;
+    };
+    author: {
+        text: string;
+        email: string;
+        href: string;
+    };
+    content: {
+        src: string;
+    };
+    preference: { [key in Preference]?: string | boolean };
+    feature: {
+        [key in Feature]?: {
+            [key: string]: string;
+        }
+    };
+    permission: Permission[];
+    access: {
+        origin: string;
+    };
+}
+
+type Permission = 'location' | 'internet';
+
+type Preference =
+    | 'appBackground'
+    | 'windowBackground'
+    | 'frameBackgroundColor'
+    | 'autoLaunch'
+    | 'autoUpdate'
+    | 'smartUpdate'
+    | 'debug'
+    | 'statusBarAppearance';
+
+type Feature = 'ajpush' | 'wx';
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,6 @@ import { Navigation } from './navigation';
 import { api as ak } from './api';
 import { akUtil } from './util';
 import { akEnv } from './env';
+import { apicloudConfig } from './apicloud.config';
 
-export { ak, Navigation, akUtil, akEnv };
+export { ak, Navigation, akUtil, akEnv, apicloudConfig };
