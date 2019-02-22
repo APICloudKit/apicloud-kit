ca996d05cfa0b631c8cb8d0c05b77530bf41e216
Kai Ye <grepug@163.com>
2019-02-22 22:11:00 +0800

    env

--- types/env.d.ts
+++ types/env.d.ts
@@ -3,14 +3,41 @@ import { AK } from './api.interface';
 export namespace akEnv {
     type Env = 'development' | 'test' | 'production' | 'preProduction';
 
+    type UrlConfig = {
+        auth: {
+            baseURL: string;
+            client_id: string;
+            client_secret: string;
+        };
+        business: {
+            baseURL: string;
+        };
+    };
+
+    type EnvStoreConfig = {
+        client: string;
+        server: UrlConfig;
+        isRemember?: boolean;
+    };
+
+    type EnvStore = {
+        current: Env;
+        config: { [key in Env]: EnvStoreConfig };
+    };
+
     declare const saveAuthToken: (
         token: string,
         authTokenKey?: string,
-    ) => AK.ApiRes<true>;
+    ) => // @bug ?
+    AK.ApiRes<true>;
 
     declare const getAuthToken: (
         authTokenKey?: string,
     ) => Promise<string | null>;
 
     declare const rmAuthToken: (authTokenKey?: string) => Promise<void | null>;
+
+    declare const getEnv: () => Promise<EnvStore | null>;
+
+    declare const setEnv: (value: EnvStore) => Promise<void>;
 }
